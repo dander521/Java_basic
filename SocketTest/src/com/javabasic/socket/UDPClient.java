f5d9a1764bc369fbeb989b21b0c882dce6af8443
Roger_Chen_max5211314 <rong123020990@126.com>
2019-06-12 16:01:50 +0800

    添加udp socket传输代码

--- SocketTest/src/com/javabasic/socket/UDPClient.java
+++ SocketTest/src/com/javabasic/socket/UDPClient.java
@@ -1,5 +1,23 @@
 package com.javabasic.socket;
 
+import java.io.IOException;
+import java.net.*;
+
 public class UDPClient {
+
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+        byte[] buff = "Hello world".getBytes();
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        DatagramPacket packet = new DatagramPacket(buff, buff.length, address, 65001);
+        socket.send(packet);
+
+
+        byte[] data = new byte[100];
+        DatagramPacket receivePacket = new DatagramPacket(data, data.length);
+        socket.receive(receivePacket);
+        String content = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        System.out.println(content);
+    }
 
 }
--- SocketTest/src/com/javabasic/socket/UDPServer.java
+++ SocketTest/src/com/javabasic/socket/UDPServer.java
@@ -1,4 +1,27 @@
 package com.javabasic.socket;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class UDPServer {
+
+    public static void main(String[] args) throws IOException {
+        // 服务端接受客户端发送的数据报
+        DatagramSocket datagramSocket = new DatagramSocket(65001); // 监听端口号
+        byte[] buff = new byte[100]; // 存储从客户端接收到的内容
+        DatagramPacket packet = new DatagramPacket(buff, buff.length);
+        // 接受客户端发送过来的内容 并将内容封装进 DatagramPacket
+        datagramSocket.receive(packet);
+
+        byte[] data = packet.getData();
+        String content = new String(data, 0, packet.getLength());
+        System.out.println(content);
+
+        // 将要发送的内容转换为二进制
+        byte[] sendedContent = String.valueOf(content.length()).getBytes();
+        // 从packet 对象获取数据来源的地址 和 端口
+        DatagramPacket packetToClient = new DatagramPacket(sendedContent, sendedContent.length, packet.getAddress(), packet.getPort());
+        datagramSocket.send(packetToClient);
+    }
 }
